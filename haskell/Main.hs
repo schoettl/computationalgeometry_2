f2e1be929633500b1518a797f8e93c53ec318d2f
Jakob Schöttl <jschoett@gmail.com>
2015-05-06 15:26:10 +0200

    usage text inline -- docopt still not working

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -5,7 +5,14 @@ import System.Environment (getArgs)
 import System.Console.Docopt
 
 patterns :: Docopt
-patterns = [docoptFile|USAGE.txt|]
+patterns = [docopt|
+usage:
+  area [ -p <pointfile> ] <areafile>
+
+options:
+  -p, --points=<pointfile>  for each point in this file,
+      test in which area it is and output the result
+|]
 
 getArgOrExit = getArgOrExitWith patterns
 
@@ -13,8 +20,8 @@ main = do
   args <- parseArgsOrExit patterns =<< getArgs
 
   areaFile <- getArgOrExit args (argument "areafile")
-  putStr =<< readFile areaFile
+  putStr =<< readFile areaFile -- = cat
 
   when (isPresent args (longOption "points")) $ do
     pointFile <- getArgOrExit args (argument "pointfile")
-    putStrLn "--points is set"
+    putStrLn $ "--points is set: " ++ pointFile
