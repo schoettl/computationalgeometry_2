2b2e96ada007d4e22dac4186d81d6fcd699940c4
Jakob Schöttl <jschoett@gmail.com>
2015-05-28 16:19:47 +0200

    Updated test script

--- test/test-brd.sh
+++ test/test-brd.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-usage="$0"
-
+usage="test/test-brd.sh"
 
 function calculateRatios() {
     awk 'NR==1{s=$2}; {print $1, $2/s}'
@@ -10,22 +9,21 @@ function calculateRatios() {
 temp=$(mktemp)
 haskell/area -p staedte.txt laender.txt > "$temp"
 
-tmpPart1=$(mktemp)
-tmpPart2=$(mktemp)
-
 sepLineNo=$(awk '/^===/{print NR}' "$temp")
-head -n $((sepLineNo-1)) "$temp" | sort | calculateRatios > "$tmpPart1"
-tail -n +$((sepLineNo+1)) "$temp" | sort > "$tmpPart2"
+head -n $((sepLineNo-1)) "$temp" | sort | calculateRatios > test/computed-part1.txt
+tail -n +$((sepLineNo+1)) "$temp" | sort > test/computed-part2.txt
 
 awk '{print $1, $2}' test/bundeslaender.txt \
 | sort \
 | calculateRatios \
 | awk '!/Bundesrepublik/' \
-| diff -u "$tmpPart1" -
-
-echo "================================================================================"
+> test/real-part1.txt
 
 awk '{print $3, "[" $1 "]"}' test/bundeslaender.txt \
 | sort \
 | awk '!/Bundesrepublik/' \
-| diff -u "$tmpPart2" -
+> test/real-part2.txt
+
+echo "Vergleiche computed-part1.txt mit real-part1.txt"
+echo "und        computed-part2.txt mit real-part2.txt"
+echo "im Ordner test/ -- zum Beispiel mit Meld"
